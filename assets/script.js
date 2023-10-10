13e1c303084e1704cb295f6ece4b1523a097d8d1
Fami <michaelfami@gmail.com>
2023-10-10 15:16:46 -0500

    quantity buttons

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,25 @@
 
 
 // Below this line is javascript for checkout page. Above this line is javascript for index page
+
+
+let deductBtnArr = document.querySelectorAll('.minus-btn');
+let addButtonArr = document.querySelectorAll('.plus-btn');
+
+deductBtnArr.forEach(deductBtn => {
+    deductBtn.addEventListener("click", function(){
+        console.log("minus clicked")
+        let currentInputBox = deductBtn.previousElementSibling
+        currentInputBox.value =  currentInputBox.value - 1;
+    } )  
+});
+
+
+addButtonArr.forEach(addButton => {
+    addButton.onclick = () => {
+        console.log("plus clicked")
+
+        let currentInputBox = addButton.nextElementSibling;
+        currentInputBox.value =  parseInt(currentInputBox.value) + 1;
+    }
+});

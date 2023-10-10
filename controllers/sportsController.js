1285429817bb363b4633f7dedc5b72e35d2c0d97
Logan <logankrieger317@gmail.com>
2023-10-10 14:35:04 -0500

    baseball by id

--- controllers/sportsController.js
+++ controllers/sportsController.js
@@ -1,8 +1,11 @@
-const Sports =require('../models/sports')
+const Sports =require('../models/index')
+const Baseball = require('../models/baseball')
+
 
 module.exports={
     getAllSports,
     getSportsById,
+    getBaseballById,
     
 }
 
@@ -23,6 +26,19 @@ async function getSportsById (req,res){
             return res.json(sport);
         }
         res.status(404).json({message:'Sport not found!'})
+    }catch(error){
+        res.status(500).json({error:error.message})
+    }
+}
+
+async function getBaseballById(req,res){
+    try{
+        const {id} = req.params;
+        const baseball = await Baseball.findById(id);
+        if(baseball){
+            return res.json(baseball);
+        }
+        res.status(404).json({message:'item not found!'})
     }catch(error){
         res.status(500).json({error:error.message})
     }

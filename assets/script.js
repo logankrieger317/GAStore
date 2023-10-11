3f805cfc1c3181d7a7d3593ca51458b49bf8842b
Fami <michaelfami@gmail.com>
2023-10-11 12:45:52 -0500

    fixed errors

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,11 @@
 
 // Below this line is javascript for checkout page. Above this line is javascript for index page
 
-
+console.log("i exist")
 let deductBtnArr = document.querySelectorAll('.minus-btn');
 let addButtonArr = document.querySelectorAll('.plus-btn');
+// console.log(deductBtnArr)
+
 
 deductBtnArr.forEach(deductBtn => {
     deductBtn.addEventListener('click', (e)=>{

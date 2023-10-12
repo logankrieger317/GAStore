f77fbcaa916c84e8247c8eaedeed3d69cd288b80
Fami <michaelfami@gmail.com>
2023-10-12 11:09:47 -0500

    deleted comments

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-const addItemsToCart = document.getElementsByClassName("add-to-cart");
-console.log(addItemsToCart);
+// const addItemsToCart = document.getElementsByClassName("add-to-cart");
+// console.log(addItemsToCart);
 
-for (let i = 0; i < addItemsToCart.length; i++) {
-  let button = addItemsToCart[i];
-  button.addEventListener("click", () => {
-    console.log("Clicked");
-  });
-}
+// for (let i = 0; i < addItemsToCart.length; i++) {
+//   let button = addItemsToCart[i];
+//   button.addEventListener("click", () => {
+//     console.log("Clicked");
+//   });
+// }
 
 // const products = [
 //     { name: "Product 1", price: 19.99 },
@@ -32,24 +32,12 @@ for (let i = 0; i < addItemsToCart.length; i++) {
 
 //   updateCart()
 
-
-
-
-
-
-
-
-
-
-
 // const getAllBaseball = async () => {
 //     const sports = await axios.get('mongodb://127.0.0.1:27017/sportsDatabase')
 //     console.log(sports)
 // }
 
 // getAllBaseball()
-
-
 
 // Below this line is javascript for checkout page. Above this line is javascript for index page
 
@@ -66,25 +54,20 @@ console.log("i exist");
 //     } )
 // });
 
-let productInfo = document.querySelector("#productInformation");
-
-
 // console.log("i exist")
 // let deductBtnArr = document.querySelectorAll('.minus-btn');
 // let addButtonArr = document.querySelectorAll('.plus-btn');
 // console.log(deductBtnArr)
-
-
-
 
 // deductBtnArr.forEach(deductBtn => {
 //     deductBtn.addEventListener('click', (e)=>{
 //         console.log("minus clicked")
 //         let currentInputBox = deductBtn.previousElementSibling
 //         currentInputBox.value =  parseInt(currentInputBox.value) - 1;
-//     } )  
+//     } )
 // });
- 
+
+let productInfo = document.querySelector("#productInformation");
 
 const getAllBaseball = async () => {
   const sports = await axios.get("http://localhost:3001/baseball");
@@ -107,80 +90,7 @@ const getAllBaseball = async () => {
   <span class="product-price">${baseballProduct.price}</span> 
   <button class="add-to-cart" data-product="Product 1" data-price="19.99">Add to Cart</button>
   </div>`;
-  
   });
 };
 
-
-
 getAllBaseball();
-
-// let getStuff = async ()=>{await axios.get(
-//     `http://localhost:3001/baseball`
-//  )}
-
-//  getStuff()
-//  console.log(response)
-
-
-// let response = await axios.get(
-//     `http://localhost:3001/baseball`
-//  )
-//  let baseballArr=response.baseballProducts
-// baseballArr.forEach(baseballProduct => {
-    
-     
-//      let { data } = baseballProduct
-//      let productPic = data.image
-//         productInfo.innerHTML = `
-//         <li>Name: ${data.product}</li>
-//         <li>Price: ${data.price}</li>
-//         `
-//      productImage.src = productPic
-// });
-
-// button2.addEventListener('click', async () => {
-//     console.log('button clicked')
-    // const actorInput = document.getElementById('actorText')
-    // const name = actorInput.value
-    // console.log(actor)
-    
-//  })
-//  console.log('working')
-
-
-
-const getBaseball = async () => {
-    const baseball = await axios.get('http://localhost:3001/baseball')
-    console.log(baseball)
- }
- 
- getBaseball()
- 
- 
- button2.addEventListener('click', async () => {
-    console.log('button clicked')
-    const actorInput = document.getElementById('actorText')
-    const name = actorInput.value
-    console.log(actor)
-    let response = await axios.get(
-       `http://localhost:3001/actors/${name}`
-    )
-    console.log(response)
-    let { data } = response
-    let productI = data.image
-       actorInfo.innerHTML = `
-       <li>Product: ${data.product}</li>
-       <li>Price: ${data.price}</li>
-       `
-    })
-
-// button2.addEventListener('click', async () => {
-//     console.log('button clicked')
-//     // const actorInput = document.getElementById('actorText')
-//     // const name = actorInput.value
-//     // console.log(actor)
-
-//  })
-//  console.log('working')
-

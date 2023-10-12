990d4e78b658ec803ba4669a2568e4c6b7c06d59
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-10-12 11:54:05 -0400

    disregard

--- assets/script.js
+++ assets/script.js
@@ -45,12 +45,12 @@ for (let i = 0; i < addItemsToCart.length; i++) {
 
 
 
-const getAllBaseball = async () => {
-    const sports = await axios.get('http://localhost:3001/baseball')
-    console.log(sports)
-}
+// const getAllBaseball = async () => {
+//     const sports = await axios.get('mongodb://127.0.0.1:27017/sportsDatabase')
+//     console.log(sports)
+// }
 
-getAllBaseball()
+// getAllBaseball()
 
 
 // Below this line is javascript for checkout page. Above this line is javascript for index page
@@ -58,47 +58,74 @@ getAllBaseball()
 
 
 
-console.log("i exist")
-let deductBtnArr = document.querySelectorAll('.minus-btn');
-let addButtonArr = document.querySelectorAll('.plus-btn');
+// console.log("i exist")
+// let deductBtnArr = document.querySelectorAll('.minus-btn');
+// let addButtonArr = document.querySelectorAll('.plus-btn');
 // console.log(deductBtnArr)
 
 
 
 
-deductBtnArr.forEach(deductBtn => {
-    deductBtn.addEventListener('click', (e)=>{
-        console.log("minus clicked")
-        let currentInputBox = deductBtn.previousElementSibling
-        currentInputBox.value =  parseInt(currentInputBox.value) - 1;
-    } )  
-});
+// deductBtnArr.forEach(deductBtn => {
+//     deductBtn.addEventListener('click', (e)=>{
+//         console.log("minus clicked")
+//         let currentInputBox = deductBtn.previousElementSibling
+//         currentInputBox.value =  parseInt(currentInputBox.value) - 1;
+//     } )  
+// });
  
 
 
 
 
-let response = await axios.get(
-    `http://localhost:3001/baseball`
- )
- let baseballArr=response.baseballProducts
-baseballArr.forEach(baseballProduct => {
+// let response = await axios.get(
+//     `http://localhost:3001/baseball`
+//  )
+//  let baseballArr=response.baseballProducts
+// baseballArr.forEach(baseballProduct => {
     
      
-     let { data } = baseballProduct
-     let productPic = data.image
-        productInfo.innerHTML = `
-        <li>Name: ${data.product}</li>
-        <li>Price: ${data.price}</li>
-        `
-     productImage.src = actorPic
-});
+//      let { data } = baseballProduct
+//      let productPic = data.image
+//         productInfo.innerHTML = `
+//         <li>Name: ${data.product}</li>
+//         <li>Price: ${data.price}</li>
+//         `
+//      productImage.src = productPic
+// });
 
-button2.addEventListener('click', async () => {
-    console.log('button clicked')
+// button2.addEventListener('click', async () => {
+//     console.log('button clicked')
     // const actorInput = document.getElementById('actorText')
     // const name = actorInput.value
     // console.log(actor)
     
- })
- console.log('working')
+//  })
+//  console.log('working')
+
+
+
+const getBaseball = async () => {
+    const baseball = await axios.get('http://localhost:3001/baseball')
+    console.log(baseball)
+ }
+ 
+ getBaseball()
+ 
+ 
+ button2.addEventListener('click', async () => {
+    console.log('button clicked')
+    const actorInput = document.getElementById('actorText')
+    const name = actorInput.value
+    console.log(actor)
+    let response = await axios.get(
+       `http://localhost:3001/actors/${name}`
+    )
+    console.log(response)
+    let { data } = response
+    let productI = data.image
+       actorInfo.innerHTML = `
+       <li>Product: ${data.product}</li>
+       <li>Price: ${data.price}</li>
+       `
+    })
